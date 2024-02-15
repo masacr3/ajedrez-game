9321070ad6c45f2bd68fc64d3baa8b29aa0510dd
masacr3 <leorhcp2030@gmail.com>
2024-02-15 15:24:51 -0300

    first commit

---
+++ src/main/java/com/masacr3/ajedrezgame/AjedrezGameApplication.java
@@ -0,0 +1,13 @@
+package com.masacr3.ajedrezgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjedrezGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjedrezGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masacr3/ajedrezgame/AjedrezGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masacr3.ajedrezgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjedrezGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
